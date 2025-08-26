7ad1d6fee72549ac41e00976194fdb1f0d3ef223
DeWeWO <dewel000per@gmail.com>
2025-08-26 17:14:59 +0500

    masofaviy talim blt

--- handlers/users/register.py
+++ handlers/users/register.py
@@ -1,5 +1,6 @@
 from aiogram import types, F, Router
 from aiogram.types import ReplyKeyboardRemove
+from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from keyboards.inline.buttons import register_confirm
 from keyboards.reply.buttons import register_markup
@@ -9,4 +10,27 @@ from loader import db
 router = Router()
 @router.message(F.text == "👤 Ro'yxatdan o'tish")
 async def start_register(message: types.Message, state: FSMContext):
-    await message.answer("Sallom)")
+    await message.answer("<b>Familiya Ism Sharifingizni to'liq kiriting:</b>\n\n<i>Na'muna: Abdullayev Abdulla Abdulla o'g'li</i>",
+                        reply_markup=ReplyKeyboardRemove
+    )
+    await state.set_state(RegisterState.fio)
+
+@router.message(StateFilter(RegisterState.fio))
+async def get_fio(message: types.Message, state: FSMContext):
+    fio = message.text.strip()
+    words = fio.split()
+    if len(words) < 2:
+        await message.answer("❌ F.I.Sh ni to'liq kirtmadingiz.")
+        return
+    await state.update_data({"fio": fio})
+    await message.answer("JSHSHIR ingizni kiriting.\n14 raqamdan iborat bo'lishi shart.")
+    await state.set_state(RegisterState.pnfl)
+
+@router.message(StateFilter(RegisterState.pnfl))
+async def get_pnfl(message: types.Message, state: FSMContext):
+    pnfl = message.text.strip()
+    if len(pnfl) == 14:
+        await message.answer("❌ JSHSHIR noto'g'ri. 14 raqamdan iborat bo'lishi shart.")
+        return
+    await state.update_data({"pnfl": pnfl})
+    await message.answer()
